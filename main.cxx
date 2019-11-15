26e74dd1c52af863282e1efe0bc292538f9bdd60
Cory Bloor <cgbloor@ucalgary.ca>
2019-11-14 22:08:13 -0700

    Add basic PLY support

--- main.cxx
+++ main.cxx
@@ -1,8 +1,12 @@
+#include <cstdarg>
 #include <cstdint>
+#include <cstdlib>
+#include <cstring>
 #include <cinttypes>
 #include <cstdio>
 #include <array>
 #include <fstream>
+#include <sstream>
 #include <iostream>
 #include <string>
 #include <vector>
@@ -37,6 +41,211 @@ bool starts_with(const char* s, const char* p) {
     }
   }
   return true;
+}
+
+void die(const char* fmt, ...) {
+  std::va_list argp;
+  va_start(argp, fmt);
+  std::vfprintf(stderr, fmt, argp);
+  va_end(argp);
+  std::exit(EXIT_FAILURE);
+}
+
+std::string read_file(const char* filename) {
+  std::ifstream in(filename, std::ios::binary | std::ios::ate);
+  const auto filesize = in.tellg();
+  in.seekg(0);
+  std::string str(filesize, '\0');
+  if (!in.read(&str[0], filesize)) {
+    die("%s: Could not read file\n", filename);
+  }
+  return str;
+}
+
+bool find_end_of(const std::string& str, const std::string& needle, size_t* result) {
+  const size_t pos = str.find(needle);
+  if (pos == std::string::npos) {
+    return false;
+  }
+  *result = pos + needle.size();
+  return true;
+}
+
+struct PlyHeader {
+  size_t vertex_count = 0;
+  size_t face_count = 0;
+
+  static const size_t vertex_size = 12;
+};
+
+PlyHeader parse_ply_header(const std::string& s, const char* filename) {
+  PlyHeader ph;
+
+  std::istringstream ss;
+  ss.str(s);
+
+  enum {
+    EXPECT_MAGIC,
+    EXPECT_FORMAT,
+    EXPECT_VERTEX_ELEMENT,
+    EXPECT_VERTEX_X,
+    EXPECT_VERTEX_Y,
+    EXPECT_VERTEX_Z,
+    EXPECT_FACE_ELEMENT,
+    EXPECT_FACE_VERTEX_INDEX,
+    EXPECT_END_HEADER,
+    EXPECT_DATA,
+  } parser_expects = EXPECT_MAGIC;
+
+  std::string line;
+  size_t line_number = 0;
+  while (std::getline(ss, line)) {
+    ++line_number;
+    switch (parser_expects) {
+      case EXPECT_MAGIC: {
+        if (line == "ply") {
+          parser_expects = EXPECT_FORMAT;
+        } else {
+          readerr(filename, line_number) << "not a PLY file" << std::endl;
+        }
+      } break;
+      case EXPECT_FORMAT: {
+        if (line == "format binary_little_endian 1.0") {
+          parser_expects = EXPECT_VERTEX_ELEMENT;
+        } else {
+          readerr(filename, line_number) << "unsupported format" << std::endl;
+        }
+      } break;
+      case EXPECT_VERTEX_ELEMENT: {
+        if (1 == std::sscanf(line.c_str(), "element vertex %zu", &ph.vertex_count)) {
+          parser_expects = EXPECT_VERTEX_X;
+        } else {
+          readerr(filename, line_number) << "unsupported vertex element" << std::endl;
+        }
+      } break;
+      case EXPECT_VERTEX_X: {
+        if (line == "property float x") {
+          parser_expects = EXPECT_VERTEX_Y;
+        } else {
+          readerr(filename, line_number) << "unsupported vertex property" << std::endl;
+        }
+      } break;
+      case EXPECT_VERTEX_Y: {
+        if (line == "property float y") {
+          parser_expects = EXPECT_VERTEX_Z;
+        } else {
+          readerr(filename, line_number) << "unsupported vertex property" << std::endl;
+        }
+      } break;
+      case EXPECT_VERTEX_Z: {
+        if (line == "property float z") {
+          parser_expects = EXPECT_FACE_ELEMENT;
+        } else {
+          readerr(filename, line_number) << "unsupported vertex property" << std::endl;
+        }
+      } break;
+      case EXPECT_FACE_ELEMENT: {
+        if (1 == std::sscanf(line.c_str(), "element face %zu", &ph.face_count)) {
+          parser_expects = EXPECT_FACE_VERTEX_INDEX;
+        } else {
+          readerr(filename, line_number) << "unsupported face element" << std::endl;
+        }
+      } break;
+      case EXPECT_FACE_VERTEX_INDEX: {
+        if (line == "property list uchar uint vertex_index") {
+          parser_expects = EXPECT_END_HEADER;
+        } else {
+          readerr(filename, line_number) << "unsupported vertex_index property" << std::endl;
+        }
+      } break;
+      case EXPECT_END_HEADER: {
+        if (line == "end_header") {
+          parser_expects = EXPECT_DATA;
+        } else {
+          readerr(filename, line_number) << "unsupported field" << std::endl;
+        }
+      } break;
+      case EXPECT_DATA: {
+        // std::getline would have returned false if end_header was correctly
+        // followed by a newline character
+        readerr(filename, line_number) << "missing newline after end_header" << std::endl;
+      } break;
+    }
+  }
+
+  if (!line.empty()) {
+    readerr(filename, line_number) << "missing newline after end_header" << std::endl;
+  }
+
+  return ph;
+}
+
+bool is_mul_safe(size_t x, size_t y) {
+  return y == 0 || x*y/y == x;
+}
+
+bool ply_idx_ok(uint32_t index, size_t vertex_count) {
+  return size_t(index) < vertex_count;
+}
+
+Mesh load_ply(const char* filename) {
+  Mesh m;
+  std::string s = read_file(filename);
+
+  size_t header_size;
+  if (!find_end_of(s, "end_header\n", &header_size)) {
+    die("%s: Couldn't find 'end_header\\n'\n", filename);
+  }
+
+  const char* const header_begin = s.data();
+//const char* const header_end = s.data() + header_size;
+  const char* const data_begin = s.data() + header_size;
+  const char* const data_end = s.data() + s.size();
+
+  PlyHeader ph = parse_ply_header(std::string(header_begin, header_size), filename);
+
+  // extract the vertex data
+  if (!is_mul_safe(ph.vertex_size, ph.vertex_count)) {
+    die("%s: Vertex count too large\n", filename);
+  }
+  size_t vertex_data_size = ph.vertex_size * ph.vertex_count;
+  size_t actual_data_size = size_t(data_end - data_begin);
+  if (actual_data_size < vertex_data_size) {
+    die("%s: Expected %zu bytes of vertex data but got %zu\n", filename,
+      vertex_data_size, actual_data_size);
+  }
+  m.vertexes.resize(ph.vertex_count);
+  std::memcpy(m.vertexes.data(), data_begin, vertex_data_size);
+
+  // extract the index data
+  const char* rpos = data_begin + vertex_data_size;
+  for (size_t i = 0; i < ph.face_count; ++i) {
+    if (data_end - rpos < 1) {
+      die("%s: Expected %zu faces but got %zu\n", filename, ph.face_count, i);
+    }
+    uint8_t index_count;
+    std::memcpy(&index_count, rpos, 1);
+    rpos += 1;
+    m.faces.push_back(index_count);
+    for (uint8_t i = 0; i < index_count; ++i) {
+      if (data_end - rpos < 4) {
+        die("%s: Expected index but reached end of file\n", filename);
+      }
+      uint32_t index;
+      std::memcpy(&index, rpos, 4);
+      rpos += 4;
+      if (!ply_idx_ok(index, ph.vertex_count)) {
+        die("%s: Invalid index (%" PRIu32 ")\n", filename, index);
+      }
+      m.indexes.push_back(index);
+    }
+  }
+
+  if (rpos != data_end) {
+    std::fprintf(stderr, "%s: Extra %td bytes at end of file\n", filename, data_end - rpos);
+  }
+
+  return m;
 }
 
 Mesh load_obj(const char* filename) {
@@ -149,7 +358,16 @@ int main(int argc, const char** argv) {
   std::ofstream out("out.abc", std::ios::binary);
   std::vector<Mesh> meshes;
   for (int i = 1; i < argc; ++i) {
-    meshes.push_back(load_obj(argv[i]));
+    const char* filename = argv[i];
+    const char* ext = std::strrchr(filename, '.');
+    if (ext && std::strcmp(ext, ".ply") == 0) {
+      meshes.push_back(load_ply(filename));
+    } else if (ext && std::strcmp(ext, ".obj") == 0) {
+      meshes.push_back(load_obj(filename));
+    } else {
+      std::fprintf(stderr, "Unknown file type: %s\n", filename);
+      return 1;
+    }
   }
   export_to_alembic(out, parameters, meshes);
   return 0;
